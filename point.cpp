92d989ddf694c4b27bd3e7c624d73fa6b08088ce
Arathus <Arathus>
2017-01-25 18:39:47 +0100

    Adding initializer and initializing + compiling

--- point.cpp
+++ point.cpp
@@ -1,21 +1,30 @@
 class Point {
 
 public:
+    Point():
+   x ( 0 ), y( 0 ), z( 0 )
+    {}
     int x;
     int y;
     int z;
 
-    Point():
-   x ( 0 ), y( 0 ), z( 0 )
-{}
 
 
 
 };
-
+Point pt;
 
 
 int main ()
 {
+
+
+
+
+
+
+
+
+
     return 0;
 }
