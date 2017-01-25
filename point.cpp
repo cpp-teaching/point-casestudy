af110c0ee44273a87a8278251265537561b510ef
Arathus <Arathus>
2017-01-25 22:03:52 +0100

    Adding an operator + thingy

--- point.cpp
+++ point.cpp
@@ -26,6 +26,24 @@ public:
     return mag;
     }
 
+
+
+
+    Point operator +(const Point& rhs) const
+    {
+
+    int new_x = rhs.x + x;
+    int new_y = rhs.y + y;
+    int new_z = rhs.z + z;
+
+    Point new_pt(new_x,new_y,new_z);
+
+    return new_pt;
+
+    }
+
+
+
 private:
     int magnitude = get_magnitude();
 
@@ -53,16 +71,20 @@ return stream;
 
 
 
-
-
 int main ()
 {
 Point pt(5,3,10);
 
+
+
 Point pt2;
 
     std::cout<<pt << std::endl<< std::endl;
-    std::cout<<pt2;
+    std::cout<<pt2<<std::endl<< std::endl;
+
+Point pt3 = pt + pt2;
+
+std::cout << pt3;
 
 
     return 0;
