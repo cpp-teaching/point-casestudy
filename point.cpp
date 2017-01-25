c40dbe776bd5a3ed7c83325e491fc7fb2319679f
Arathus <Arathus>
2017-01-25 21:11:18 +0100

    Adding default initializer

--- point.cpp
+++ point.cpp
@@ -4,8 +4,14 @@
 class Point {
 
 public:
+
+      Point():
+    x ( 0 ), y( 0 ), z( 0 )
+    {}
+
+
     Point(int x,int y,int z):
-   x ( x ), y( y ), z( z )
+    x ( x ), y( y ), z( z )
     {}
     int x;
     int y;
